f4f7dbf532e5a6fd7298a9d9f033e3e4e58aaaf4
Sagnik Roy <sagnikr95@gmail.com>
2020-03-29 19:59:00 -0400

    Deleted init.sql
    Init file useless as te initialization was done in the terminal itself.

--- server/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE user_emails (
-  ID SERIAL PRIMARY KEY,
-  username VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL
-);
-
-INSERT INTO user_emails (username, email)
-VALUES  ('Jeffrey Bridges', 'jb2099@gmail.com');
