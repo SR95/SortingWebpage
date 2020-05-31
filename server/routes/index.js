9999fa9c9cad83da4cbea0fa8e5e08889c4661a4
Sagnik Roy <sagnikr95@gmail.com>
2020-05-31 15:33:00 -0400

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/welcome", function(req, res, next) {
+  console.log("sent welcome text!");
   res.status(200).send({ welcomeMessage: "Step 1 (completed)" });
 });
 
