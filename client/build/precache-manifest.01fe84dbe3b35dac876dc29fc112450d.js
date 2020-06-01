9aaafb9ca13f41cbe8bbf46f5a7f473b4daed9e5
Sagnik Roy <sagnikr95@gmail.com>
2020-05-31 20:28:29 -0400

    Ran npm build on frontend

---
+++ client/build/precache-manifest.01fe84dbe3b35dac876dc29fc112450d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d9efc6a00b0ab0963cc6c34c4b8dc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d9bb0cc7c4d2366b147",
+    "url": "/static/css/main.4c06968d.chunk.css"
+  },
+  {
+    "revision": "db19761a7ced249228fc",
+    "url": "/static/js/2.5ed7fe5a.chunk.js"
+  },
+  {
+    "revision": "7e402396ca428a5bd514c48df615b24e",
+    "url": "/static/js/2.5ed7fe5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d9bb0cc7c4d2366b147",
+    "url": "/static/js/main.a5e6a84a.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
